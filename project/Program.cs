6424c6d6eded410ab13cc7c4fcd1ed9a1ec4aa79
Olga Nazarova <mazon2010@yandex.ru>
2022-04-04 17:11:25 +0300

    added reshenie

--- project/Program.cs
+++ project/Program.cs
@@ -10,6 +10,19 @@ namespace project
     {
         static void Main(string[] args)
         {
+            string[] array = { "Мой", "папа", "никогда", "не", "был", "в", "Египте", "но", "всегда", "мечтал", "там", "побывать" };
+            var arrayOne = new string[array.Length];
+            var size = 0;
+
+            foreach (var value in array)
+            {
+                if (value.Length <= 3)
+                {
+                    arrayOne[size] = value;
+                    size++;
+                }
+            }
+            Console.Write(string.Join(Environment.NewLine, arrayOne, 0, size));
         }
     }
 }
